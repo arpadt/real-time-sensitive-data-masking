bb08a499a70ec8417db64d803907388fbda49598
Arpad Toth <a.arpad.toth@gmail.com>
2025-02-17 14:17:30 +0100

    Initial commit

---
+++ test/macie.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Macie from '../lib/macie-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/macie-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Macie.MacieStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
